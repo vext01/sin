1cdc7c1f0b3866bd5221221051bad9af92a0363e
vext01 <vext01@gmail.com>
2012-05-05 15:06:58 +0100

    pinmap and representation of ym2612

--- fm-of-sin/fm-of-sin.ino
+++ fm-of-sin/fm-of-sin.ino
@@ -18,8 +18,38 @@
 extern "C" {
 #endif
 
+/* Pin Mapping */
+#define YMPIN_D0		0
+#define YMPIN_D1		1
+#define YMPIN_D2		2
+#define YMPIN_D3		3
+#define YMPIN_D4		4
+#define YMPIN_D5		5
+#define YMPIN_D6		6
+#define YMPIN_D7		7
+
+#define YMPIN_WR		8
+#define YMPIN_RD		9
+#define YMPIN_CS		11
+#define YMPIN_A0		10
+#define YMPIN_A1		12
+
+/* no need to be mega efficient (yet) */
+struct ym2612 {
+	uint8_t		data[8];
+	uint8_t		wr;
+	uint8_t		rd;
+	uint8_t		cs;
+	uint8_t		a0;
+	uint8_t		a1;
+};
+
 void setup(void) {
-	/* your code here */
+	int			i;
+
+	for (i = 0; i < 12; i++)
+		pinMode(i,  OUTPUT);
+
 	return;
 }
 
