f6c93f09b1a90fd604a98c73dabe5247510ca987
vext01 <vext01@gmail.com>
2012-05-26 13:30:42 +0100

    start work on a software sequencer for the sin instrument

---
+++ studio/sinstudio.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import urwid
+import sys
+import os
+
+class SinError(Exception):
+    pass
+
+class Channel:
+    """ Represents a single channel in the tracker interface """
+
+    def __init__(self, vchan_num, device, dchan_num, parent):
+        self.device = device        # serial device node
+        self.vchan_num = vchan_num  # virtual channel
+        self.dchan_num = dchan_num  # device channel
+        self.parent = parent        # parent UI instance
+
+        # make channel widget
+        self.title_w = urwid.Text("%d:%s(%d)" %
+                (self.dchan_num, self.device, self.vchan_num))
+        self.timeline_w = urwid.Text("...\n...\n...\n...\n") # XXX
+        self.pile_w = urwid.Pile([self.title_w, self.timeline_w])
+
+    def get_widget(self):
+        return self.pile_w
+
+    def update_timeline(self, keyons):
+        text = ""
+        for k in keyons:
+            if k == None:   # No key fires here
+                text += ".....\n"
+            else:
+                raise SinError("Unknown keyon event")
+
+        self.timeline_w.set_text(text)
+
+class Pattern:
+
+    def __init__(self, pat_no, length, parent_ui):
+        self.length = length
+        self.parent_ui = parent_ui
+
+        self.pattern_data = {} # chan -> keyons
+
+        # pattern starts empty
+        for c in parent_ui.chans:
+            self.pattern_data[c.vchan_num] = [ None for x in range(length) ]
+
+class UI:
+    """ The main user interface """
+
+    def __init__(self):
+        self.chans = []
+        for i in range(6):
+            self.chans.append(Channel(i, "cuaU0", i, self))
+
+        self.chan_container_w = urwid.Columns([x.get_widget() for x in self.chans])
+        self.filler_w = urwid.Filler(self.chan_container_w, valign="top")
+        self.loop = urwid.MainLoop(self.filler_w)
+
+        # XXX just one pattern for now
+        self.patterns = [ Pattern(0, 16, self) ]
+        self.cur_pattern = self.patterns[0]
+        self.update()
+
+    # XXX needs a scrollable viewport
+    def update(self):
+
+        # update channel timelines
+        pat = self.cur_pattern
+
+        for c in self.chans:
+            keyons = pat.pattern_data[c.vchan_num]
+            c.update_timeline(keyons)
+
+    def run(self):
+        self.loop.run()
+
+if __name__ == "__main__":
+    u = UI()
+    u.run()
